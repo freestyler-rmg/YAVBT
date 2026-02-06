0b4a9f6db52bf83ef39ce86ab3bf310a0c5b7f7a
Roy Margasa <roy.margasa@gmail.com>
2026-02-06 16:58:18 +0700

    Added env file

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   scrollBehavior() {
     return { top: 0 };
   },
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL || '/YAVBT/'),
   routes
 });
 
